b3d5c89b9be4d99059cb5dd7089b86257413a96e
Anastasia <anastasia.hrynkevich@outlook.com>
2023-06-02 23:55:48 +0100

    fixed mistype

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <footer>
-            <p className="footer-info width-container">©202 Anastasia Hrynkevich. All rights reserved.</p>
+            <p className="footer-info width-container">©2023 Anastasia Hrynkevich. All rights reserved.</p>
         </footer>
     )
 }
